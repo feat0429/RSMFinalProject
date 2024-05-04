c56b820f45de8e9539e130f62432b5e303b0e8a3
Fredy Amaya <feat0429f@gmail.com>
2024-05-03 23:17:31 -0600

    Deactivated tanstack query retry.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,8 @@ import './index.css'
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false
+      refetchOnWindowFocus: false,
+      retry: false
     }
   }
 })
